b420410ccce48b899ae03c9fd62fcc1bc4675702
carry-chen <chenxxx1204@gmail.com>
2025-05-11 00:21:31 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -30,4 +30,7 @@ def webhook():
                 "text": reply_text
             })
     return jsonify({"messages": messages})
-    
+    if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
