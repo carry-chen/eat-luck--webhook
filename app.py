1e90a4848cbbf06ea747eb4298ec06b1c9c69aa6
carry-chen <chenxxx1204@gmail.com>
2025-05-11 15:30:34 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, jsonify
 import openai
 import os
@@ -30,7 +29,9 @@ def webhook():
                 "text": reply_text
             })
     return jsonify({"messages": messages})
-    if __name__ == '__main__':
+
+
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
 
